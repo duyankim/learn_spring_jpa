ab27540d336c6523fe797284ada39564d6a09b09
duyankim <arbolkim@gmail.com>
2023-06-02 17:20:46 +0900

    chore: config fixed

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,4 +1,4 @@
-y package jpabook.jpashop;
+package jpabook.jpashop;
 
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
@@ -6,10 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
