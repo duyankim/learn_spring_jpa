ab2845856d814bd4c9172d0de21708fad2398255
duyankim <arbolkim@gmail.com>
2023-06-05 17:31:33 +0900

    feat: Delivery entity 추가
    - 주문시 하나의 배송 정보를 생성
    - Delivery와 Order는 1:1관계
    - DeliveryStatus는 중간에 Enum이 추가 되어 코드 값이 변경되면 장애가 발생할 수 있으므로 Ordinal이 아닌 String으로 저장

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -13,7 +13,7 @@ public abstract class Item {
 
     @Id @GeneratedValue
     @Column(name = "item_id")
-    private long id;
+    private Long id;
 
     private String name;
     private int price;
